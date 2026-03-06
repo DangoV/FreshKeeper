8c12faa1d362447d451193aa92d486af66c07d9a
DangoV <62790486+DangoV@users.noreply.github.com>
2026-03-06 20:17:29 +0300

    Fix missing Material3 XML theme dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
     implementation("androidx.compose.ui:ui")
